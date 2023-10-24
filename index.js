520b1e94b28aac88b49b62a556101b1dc48baaa5
Rahatul-Alom <muhammadrahatulalom@gmail.com>
2023-10-25 05:06:37 +0600

    adde env file and sucre user name and password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,7 +13,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Brand-shop:wHu0fAfXWitT3rIp@cluster0.cep75go.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cep75go.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +29,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const brandCollection = client.db('brandDB').collection('brand')
+
+    app.get('/brand', async (req, res)=>{
+      const cusor = brandCollection.find();
+      const result = await cusor.toArray();
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
